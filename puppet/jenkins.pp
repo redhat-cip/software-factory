0d693f470892916f8e5129843f63694676b5f77e
Tristan Cacqueray <tristan.cacqueray@enovance.com>
2014-02-05 14:07:37 +0100

    Add jenkins-job-builder to edeploy role

--- puppet/jenkins.pp
+++ puppet/jenkins.pp
@@ -1,9 +1,14 @@
 class jenkins {
   file {'/var/lib/jenkins/config.xml':
     ensure  => file,
-    mode => 0644,
-    owner => "jenkins",
-    group => "nogroup",
-    content => template('jenkins/config.xml'),
+    mode    => 0644,
+    owner   => "jenkins",
+    group   => "nogroup",
+    content => template('jenkins/config.xml.erb'),
+  }
+  file {'/etc/jenkins_jobs/jenkins_jobs.ini':
+    ensure  => file,
+    mode    => '0400',
+    content => template('jenkins/jenkins_jobs.ini.erb'),
   }
 }
