68882b39857316ddcf5bec148d705e9d7de36ebc
Fabien Boucher <fabien.boucher@enovance.com>
2014-02-05 14:58:42 +0100

    Tool to create Gerrit repo is usable now
    - Create Gerrit Repo
    - Create Gerrit core Group
    - Push custom ACLs
    - Push .gitreview file

--- sf-tools/tool.py
+++ sf-tools/tool.py
@@ -1,21 +1,17 @@
 #!/bin/env/python
 
-import os
-import random
-import string
-import sys
-import shutil
-from gerritlib import gerrit
 from dulwich import client
-from dulwich import repo
 from dulwich import index
+from dulwich import repo
+from gerritlib import gerrit
 
+import os
+import shutil
+import sys
 
-#TODO(fbo): Use Dulwich to push the project ACL
-#TODO(fbo): Push the .gitreview file
 #TODO(fbo): Use yaml file to describe a project
 
-client.get_ssh_vendor = lambda: CustomSSHVendor(sys.argv[1])
+
 
 class CustomSSHVendor(client.SubprocessSSHVendor):
     """SSH vendor that shells out to the local 'ssh' command."""
@@ -31,11 +27,11 @@ class CustomSSHVendor(client.SubprocessSSHVendor):
         if username is not None:
             host = '%s@%s' % (username, host)
         args.append(host)
-        print args
         proc = subprocess.Popen(args + command,
                                 stdin=subprocess.PIPE,
                                 stdout=subprocess.PIPE)
         return client.SubprocessWrapper(proc)
+
 
 class CustomGerritClient(gerrit.Gerrit):
     def createProject(self, project, require_change_id=True):
@@ -46,11 +42,19 @@ class CustomGerritClient(gerrit.Gerrit):
         out, err = self._ssh(cmd)
         return err
 
+    def getGroupUUID(self, name):
+        cmd = 'gerrit ls-groups -v -m %s' % name
+        out, err = self._ssh(cmd)
+        uuid = out.split('\t')[1]
+        return uuid
+
+
 class GerritRepo(object):
-    def __init__(self, ssh_client, name):
+    def __init__(self, ssh_client, name, email):
         self.ssh_client = ssh_client
         self.name = name
         self.localcopy_path = '/tmp/clone-%s' % self.name
+        self.email = email
         self.fetch_project()
 
     def determine_wants(self, *args):
@@ -66,10 +70,10 @@ class GerritRepo(object):
         exclude = [f for f in os.listdir(self.local_repo.path)
                    if f != '.git' and f != path]
         if exclude:
-            exclude = exclude[0]
-            os.unlink(os.path.join(self.local_repo.path, exclude))
-            stages.append(exclude)
-            
+            for exc in exclude:
+                os.unlink(os.path.join(self.local_repo.path, exc))
+                stages.append(exc)
+
         index.build_index_from_tree(self.local_repo.path,
                                     indexfile,
                                     self.local_repo.object_store,
@@ -78,21 +82,24 @@ class GerritRepo(object):
                           path), 'w').write(content)
         self.local_repo.stage(stages)
         self.local_repo.do_commit(message,
-                            'fabien.boucher <fabien.boucher@enovance.com>',
-                            ref=ref)
+                                  self.email,
+                                  ref=ref)
         self.ssh_client.send_pack(
             self.name,
             self.determine_wants,
             self.local_repo.object_store.generate_pack_contents)
 
-
     def push_acls(self, acls):
         self._push("refs/meta/config", "project.config",
                    acls, "Update ACLs")
 
-    def push_gitreview(self):
+    def push_groups(self, groups):
+        self._push("refs/meta/config", "groups",
+                   groups, "Update Groups")
+
+    def push_gitreview(self, gitreview):
         self._push("refs/heads/master", ".gitreview",
-                   "", "Provide .gitreview file")
+                   gitreview, "Provide .gitreview file")
 
     def fetch_project(self):
         if os.path.isdir(self.localcopy_path):
@@ -102,37 +109,66 @@ class GerritRepo(object):
         self.local_repo["refs/meta/config"] = remote_refs["refs/meta/config"]
         self.local_repo["refs/heads/master"] = remote_refs["refs/heads/master"]
 
+
 def create_project(project):
     name = project['name']
-    group = project['core-group-name']
+    group = project['core-group']
     gerrit_client.createGroup(group,
-                       visible_to_all=True)
+                              visible_to_all=True)
     gerrit_client.createProject(name,
-                         require_change_id=True)
+                                require_change_id=True)
 
 if __name__ == "__main__":
-    keyfile = sys.argv[1]
+    try:
+        project_name = sys.argv[1]
+        keyfile = os.environ['SSH_KEY']
+        adminuser = os.environ['ADMIN_USER']
+        adminemail = os.environ['ADMIN_EMAIL']
+        gerrithost = os.environ['GERRIT_HOST']
+    except KeyError, e:
+        print e
+        sys.exit(1)
+
+    client.get_ssh_vendor = lambda: CustomSSHVendor(keyfile)
+
+    print "Will create project %s" % project_name
     print "Using private key : %s" % keyfile
+    print "Using username : %s" % adminuser
+    print "Using email : %s" % adminemail
+    print "Using gerrit hostname : %s" % gerrithost
 
-    gerrit_client = CustomGerritClient('198.154.188.171',
-                        'fabien.boucher',
-                        keyfile=keyfile)
-    
-    ssh_client = client.SSHGitClient('198.154.188.171',
+    gerrit_client = CustomGerritClient(gerrithost,
+                                       adminuser,
+                                       keyfile=keyfile)
+
+    ssh_client = client.SSHGitClient(gerrithost,
                                      29418,
-                                     'fabien.boucher')
+                                     adminuser)
 
-    seed = [random.choice(string.ascii_letters) for n in xrange(3)]
-    seed = "p-" + "".join(seed)
-    print seed
+    project = {'name': project_name,
+               'core-group': '%s-core' % project_name,
+               'gerrit-host': gerrithost,
+               'gerrit-host-port': '29418',
+               }
 
-    project = {'name': seed,
-               'core-group-name' : '%s-core' % seed,
-               'ACL-file-path': 'default-project.config'
-              }
-
+    sys.stdout.write("Create project %s ... " % project_name)
     create_project(project)
-    grepo = GerritRepo(ssh_client, seed)
-    acls = file('project.config').read()
-    grepo.push_acls(acls) 
-    grepo.push_gitreview() 
+    sys.stdout.write("done.\n")
+    sys.stdout.write("Retrieve groups UUID ... ")
+    project['core-group-uuid'] = \
+        gerrit_client.getGroupUUID(project['core-group'])
+    project['non-interactive-users'] = \
+        gerrit_client.getGroupUUID('Non-Interactive')
+    sys.stdout.write("done.\n")
+    sys.stdout.write("Clone previously created repo ... ")
+    email = "%s <%s>" % (adminuser, adminemail)
+    grepo = GerritRepo(ssh_client, project_name, email)
+    sys.stdout.write("done.\n")
+    acls = file('templates/project.config').read() % project
+    groups = file('templates/groups').read() % project
+    gitreview = file('templates/gitreview').read() % project
+    sys.stdout.write("Add ACLs, groups files, .gitreview files ... ")
+    grepo.push_groups(groups)
+    grepo.push_acls(acls)
+    grepo.push_gitreview(gitreview)
+    sys.stdout.write("done.\n")
