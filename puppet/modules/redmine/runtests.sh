7efb81306d1909fc1cd99e6e7e3ade56b3dcad91
Christian Schwede <christian.schwede@enovance.com>
2014-02-06 20:53:34 +0000

    Remove unused Gemfile, update runtests to use 'rake rspec'

--- puppet/modules/redmine/runtests.sh
+++ puppet/modules/redmine/runtests.sh
@@ -3,9 +3,11 @@
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
 
 rvm use $ruby@$$ --create
-gem install bundler
-bundle install 
-bundle exec rspec spec
+gem install --no-rdoc --no-ri puppet-lint
+gem install --no-rdoc --no-ri rspec-puppet
+gem install --no-rdoc --no-ri rake
+gem install --no-rdoc --no-ri puppet
+rake rspec
 RESULT=$?
 rvm --force gemset delete $$
 exit $RESULT
