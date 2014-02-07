1fb665ff277dbc2da5df56fd000c389e922a4ab4
Christian Schwede <christian.schwede@enovance.com>
2014-02-07 15:23:03 +0000

    Add serverspec tests for jenkins

---
+++ serverspec/spec/jenkins/jenkins_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe user('jenkins') do
+    it {
+        should exist 
+    }
+end
+
+describe file('/var/lib/jenkins/config.xml') do
+    it {
+        should be_file
+        should be_owned_by 'jenkins'
+        should be_grouped_into 'nogroup'
+        should be_mode '644'
+    }
+end
+
+describe file('/etc/jenkins_jobs/jenkins_jobs.ini') do
+    it {
+        should be_file
+        should be_owned_by 'root'
+        should be_grouped_into 'root'
+        should be_mode '400'
+    }
+end
+
+describe package('jenkins') do
+    it { should be_installed }
+end
+
+describe service('jenkins') do
+  it { should be_enabled }
+end
