902cfac905963f3fd7d52ccab5f125049464c2a6
Tristan Cacqueray <tristan.cacqueray@enovance.com>
2014-02-10 17:37:28 +0100

    Add gerrit-trigger gerrit server configuration

--- puppet/modules/jenkins/manifests/init.pp
+++ puppet/modules/jenkins/manifests/init.pp
@@ -6,6 +6,11 @@ class jenkins ($settings = hiera_hash('jenkins', '')) {
     group   => "nogroup",
     content => template('jenkins/config.xml.erb'),
   }
+  file {'/var/lib/jenkins/gerrit-trigger.xml':
+    ensure  => file,
+    mode    => '0644',
+    content => template('jenkins/gerrit-trigger.xml.erb'),
+  }
   file {'/etc/jenkins_jobs/jenkins_jobs.ini':
     ensure  => file,
     mode    => '0400',
