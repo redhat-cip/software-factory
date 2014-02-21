05d237c3592daa2deb3b60fa11eece9d4ef460c0
Tristan Cacqueray <tristan.cacqueray@enovance.com>
2014-02-21 12:51:09 +0100

    Refactor edeploy/bootstrap.sh to use function.sh
    This have not been tested...
    
    Change-Id: I41fc821351229a2aaf9d34adc4465c592dcee1a4

--- edeploy/bootstrap.sh
+++ edeploy/bootstrap.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
+
+. ../function.sh
+
 TIMESTAMP=`date +"%Y%m%d"`
 PREFIX="edeploy-$TIMESTAMP-"
 VM_FLAVOR="10"
 ROLES="puppetmaster mysql ldap redmine jenkins gerrit "
 RELEASE="D7-H.1.0.0"
-HOSTS_YAML="../puppet/hiera/hosts.yaml"
-BUILD="../build"
 SUMMARY=""
-
-mkdir -p ${BUILD}/cloudinit
-
-echo -e "hosts:\n  localhost:\n    ip: 127.0.0.1" > $HOSTS_YAML
-
-
-cp ../serverspec/hosts.yaml.tpl ../serverspec/hosts.yaml
-echo > hosts
 
 SECURITY_GROUP="${PREFIX}sg"
 nova secgroup-create $SECURITY_GROUP "Software Factory"
@@ -24,114 +17,92 @@ nova secgroup-add-rule $SECURITY_GROUP tcp 22 22 46.231.128.0/24
 ***REMOVED***
 nova secgroup-add-rule $SECURITY_GROUP tcp 29418 29418 94.143.116.0/24  # VPN
 nova secgroup-add-rule $SECURITY_GROUP tcp 29418 29418 46.231.128.0/24
-nova secgroup-add-rule $SECURITY_GROUP tcp 80 80 0.0.0.0/0 
-nova secgroup-add-rule $SECURITY_GROUP tcp 8080 8080 0.0.0.0/0 
+nova secgroup-add-rule $SECURITY_GROUP tcp 80 80 0.0.0.0/0
+nova secgroup-add-rule $SECURITY_GROUP tcp 8080 8080 0.0.0.0/0
 
-PUPPETMASTER_IP=""
+# This make sure ${BUILD} is clean
+new_build
 
+# VM boot
 for ROLENAME in $ROLES; do
-	VM_NAME="$PREFIX$ROLENAME"
+    VM_NAME="$PREFIX$ROLENAME"
 
     echo "Processing $VM_NAME"
 
-	if [ "$1" == "recreate" ]; then	
-		rm /var/lib/debootstrap/install/D7-H.1.0.0/$ROLENAME-D7-H.1.0.0.img*
-		rm /var/lib/debootstrap/install/D7-H.1.0.0/$ROLENAME.done
-	fi
-
-	if [ ! -e "/var/lib/debootstrap/install/D7-H.1.0.0/$ROLENAME-D7-H.1.0.0.img.qcow2" ]; then
-		make $ROLENAME
-	fi
-
-	IMAGE_ID=`glance image-list | grep $PREFIX$ROLENAME  | cut -f 2 -d " "`
-	
-	if [ "$IMAGE_ID" == "" ]; then
-		echo "Registering image in Glance..."
-
-		GLANCE_OUTPUT=`glance -v image-create --name="$VM_NAME" --container-format ovf --disk-format qcow2 < /var/lib/debootstrap/install/$RELEASE/$ROLENAME-$RELEASE.img.qcow2`
-		
-		# Get image ID
-		IMAGE_ID=`echo  $GLANCE_OUTPUT | grep -ohe "id | [0-9a-f-]\{36\}" | cut -c 6-`
-	fi 
-
-	echo "Booting new VM..."
-
-    if [ "$PUPPETMASTER_IP" != "" ]; then
-        cat ../cloudinit/$ROLENAME.cloudinit | sed -i -e "s#PUPPETMASTER#$PUPPETMASTER_IP#g" > ${BUILD}/cloudinit/$ROLENAME.cloudinit
-    else
-        cp ../cloudinit/$ROLENAME.cloudinit ${BUILD}/cloudinit/$ROLENAME.cloudinit
+    if [ "$1" == "recreate" ]; then
+        rm /var/lib/debootstrap/install/D7-H.1.0.0/$ROLENAME-D7-H.1.0.0.img*
+        rm /var/lib/debootstrap/install/D7-H.1.0.0/$ROLENAME.done
     fi
 
-	# Boot new VM with that image
-	NOVA_OUTPUT=`nova boot  --flavor $VM_FLAVOR --image $IMAGE_ID --user-data ${BUILD}/cloudinit/$ROLENAME.cloudinit --security-groups $SECURITY_GROUP $VM_NAME`
+    if [ ! -e "/var/lib/debootstrap/install/D7-H.1.0.0/$ROLENAME-D7-H.1.0.0.img.qcow2" ]; then
+        make $ROLENAME
+    fi
 
-	VM_ID=`echo $NOVA_OUTPUT | grep -ohe "id | [0-9a-f-]\{36\}" | cut -c 6-`
-	
-	echo "Getting private IP..."
-	RETRIES=0
-	while [ -z $IP ]; do
-		IP=`nova show $VM_ID | grep "network" | cut -d "|" -f 3 | sed 's/ *$//g'`
-		let RETRIES=RETRIES+1
-		if [ "$RETRIES" == "9" ]; then
-			echo "Failed getting private IP"
-			break
-		fi
-		sleep 10
-	done 
+    IMAGE_ID=`glance image-list | grep $PREFIX$ROLENAME  | cut -f 2 -d " "`
+
+    if [ "$IMAGE_ID" == "" ]; then
+        echo "Registering image in Glance..."
+
+        GLANCE_OUTPUT=`glance -v image-create --name="$VM_NAME" --container-format ovf --disk-format qcow2 < /var/lib/debootstrap/install/$RELEASE/$ROLENAME-$RELEASE.img.qcow2`
+
+        # Get image ID
+        IMAGE_ID=`echo  $GLANCE_OUTPUT | grep -ohe "id | [0-9a-f-]\{36\}" | cut -c 6-`
+    fi
+
+    echo "Booting new VM..."
+
+    if [ "$ROLENAME" == "puppetmaster" ]; then
+        # puppetmaster cloudinit file can be used directly
+        cp ../cloudinit/puppetmaster.cloudinit ${BUILD}/cloudinit/puppetmaster.cloudinit
+    fi
+
+    # Boot new VM with that image
+    NOVA_OUTPUT=`nova boot  --flavor $VM_FLAVOR --image $IMAGE_ID --user-data ${BUILD}/cloudinit/$ROLENAME.cloudinit --security-groups $SECURITY_GROUP $VM_NAME`
+
+    VM_ID=`echo $NOVA_OUTPUT | grep -ohe "id | [0-9a-f-]\{36\}" | cut -c 6-`
+
+    echo "Getting private IP..."
+    RETRIES=0
+    while [ -z $IP ]; do
+        IP=`nova show $VM_ID | grep "network" | cut -d "|" -f 3 | sed 's/ *$//g'`
+        let RETRIES=RETRIES+1
+        if [ "$RETRIES" == "9" ]; then
+            echo "Failed getting private IP"
+            break
+        fi
+        sleep 10
+    done
 
     RETRIES=0
     while true; do
-	    echo "Getting unused floating IP..."
-	    FLOATING_IP=`nova floating-ip-list | grep "None" | grep -ohe "[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}" | head -n 1`
-	    nova add-floating-ip $VM_ID $FLOATING_IP
+        echo "Getting unused floating IP..."
+        FLOATING_IP=`nova floating-ip-list | grep "None" | grep -ohe "[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}" | head -n 1`
+        nova add-floating-ip $VM_ID $FLOATING_IP
 
         if [ $? -eq 0 ]; then
-	        echo "Assigned floating IP $FLOATING_IP."
+            echo "Assigned floating IP $FLOATING_IP."
             SUMMARY=${SUMMARY}"$ROLENAME: $FLOATING_IP\n"
             break
         fi
+        # Store ip configuration for postconfigure
+        # This generate the same description as sf-lxc.yaml
+        putip_to_yaml "${ROLENAME}" "${FLOATING_IP}"
 
-		let RETRIES=RETRIES+1
-		if [ "$RETRIES" == "9" ]; then
-			break
-		fi
-		echo "Failed getting floating IP, will retry in 30 seconds"
-		sleep 30
-    done
-
-	echo "  $ROLENAME.pub:" >> $HOSTS_YAML
-	echo "    ip: $FLOATING_IP" >> $HOSTS_YAML
-	echo "    host_aliases: [\'sf-$ROLENAME\', \'$ROLENAME.pub\']" >> $HOSTS_YAML
-    sed -i -e "s/$ROLENAME\_ip/$FLOATING_IP/g" ../serverspec/hosts.yaml
-
-    echo "$FLOATING_IP" >> hosts
-    nova secgroup-add-rule $SECURITY_GROUP tcp 1 65535 "$FLOATING_IP/32"
-
-    # First role is puppetmaster -> first IP is from puppetmaster
-    if [ "$PUPPETMASTER_IP" == "" ]; then
-        PUPPETMASTER_IP=$FLOATING_IP
-    fi
-
-    ssh-keygen -f "$HOME/.ssh/known_hosts" -R $FLOATING_IP
-    
-    RETRIES=0
-    while true; do
-	    echo "Starting ssh-keyscan on $FLOATING_IP"
-        KEY=`ssh-keyscan -H $FLOATING_IP`
-        if [ "$KEY" != ""  ]; then
-            echo $KEY >> $HOME/.ssh/known_hosts
+        let RETRIES=RETRIES+1
+        if [ "$RETRIES" == "9" ]; then
             break
         fi
-
-		let RETRIES=RETRIES+1
-		if [ "$RETRIES" == "9" ]; then
-			break
-		fi
-		echo "ssh-keyscan on $FLOATING_IP failed, will retry in 30 seconds"
-		sleep 30
+        echo "Failed getting floating IP, will retry in 30 seconds"
+        sleep 30
     done
-    
-    scp $HOSTS_YAML $PUPPETMASTER_IP:/etc/puppet/hiera/hosts.yaml
-done 
+
+    nova secgroup-add-rule $SECURITY_GROUP tcp 1 65535 "$FLOATING_IP/32"
+
+    if [ "$ROLENAME" == "puppetmaster" ]; then
+        generate_cloudinit
+    fi
+done
+
+sf_postconfigure
 
 echo -e $SUMMARY
