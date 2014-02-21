504fa414e6170fe3ef4b57d69b273d9d7b7e0346
Fabien Boucher <fabien.boucher@enovance.com>
2014-02-21 19:52:56 +0100

    Add the option to create the config repo (jjb)
    Change-Id: Ic08a7deb7994ef63fb0d3c216536231b6ca445a7

--- tools/managesf/managesf.py
+++ tools/managesf/managesf.py
@@ -3,6 +3,7 @@ from dulwich import index
 from dulwich import repo
 from gerritlib import gerrit
 
+import re
 import os
 import shutil
 import sys
@@ -58,23 +59,31 @@ class GerritRepo(object):
         refs["refs/heads/master"] = self.local_repo.refs["refs/heads/master"]
         return refs
 
-    def _push(self, ref, path, content, message):
+    def _push(self, ref, path, content, message, clean):
         indexfile = self.local_repo.index_path()
         tree = self.local_repo[ref].tree
         stages = [path]
-        exclude = [f for f in os.listdir(self.local_repo.path)
-                   if f != '.git' and f != path]
-        if exclude:
-            for exc in exclude:
-                os.unlink(os.path.join(self.local_repo.path, exc))
-                stages.append(exc)
+        if clean:
+	    exclude = [f for f in os.listdir(self.local_repo.path)
+	               if f != '.git' and f != path]
+	    if exclude:
+                for exc in exclude:
+                    os.unlink(os.path.join(self.local_repo.path, exc))
+                    stages.append(exc)
 
         index.build_index_from_tree(self.local_repo.path,
                                     indexfile,
                                     self.local_repo.object_store,
                                     tree)
+        
+        if path.split('/') > 1:
+            d = re.sub(os.path.basename(path), '', path)
+            try:
+                os.makedirs(os.path.join(self.local_repo.path, d))
+            except OSError:
+                pass
         file(os.path.join(self.local_repo.path,
-                          path), 'w').write(content)
+              	      path), 'w').write(content)
         self.local_repo.stage(stages)
         self.local_repo.do_commit(message,
                                   self.email,
@@ -84,18 +93,9 @@ class GerritRepo(object):
             self.determine_wants,
             self.local_repo.object_store.generate_pack_contents)
 
-    def push_acls(self, acls):
-        self._push("refs/meta/config", "project.config",
-                   acls, "Update ACLs")
-
-    def push_groups(self, groups):
-        self._push("refs/meta/config", "groups",
-                   groups, "Update Groups")
-
-    def push_gitreview(self, gitreview):
-        self._push("refs/heads/master", ".gitreview",
-                   gitreview, "Provide .gitreview file")
-
+    def push_file(self, branch, filename, data, clean=False):
+        self._push(branch, filename, data, "Update %s" % filename, clean)
+    
     def fetch_project(self):
         if os.path.isdir(self.localcopy_path):
             shutil.rmtree(self.localcopy_path)
@@ -110,7 +110,6 @@ def create_project(gerrit_client, infos):
                               visible_to_all=True)
     gerrit_client.createProject(infos['name'],
                                 require_change_id=True)
-
 
 def init_gerrit_project(gerrit_client, ssh_client, infos):
     sys.stdout.write("Create project %s ... " % infos['name'])
@@ -130,12 +129,28 @@ def init_gerrit_project(gerrit_client, ssh_client, infos):
     groups = file('templates/groups').read() % infos
     gitreview = file('templates/gitreview').read() % infos
     sys.stdout.write("Add ACLs, groups files, .gitreview files ... ")
-    grepo.push_groups(groups)
-    grepo.push_acls(acls)
-    grepo.push_gitreview(gitreview)
+    grepo.push_file("refs/meta/config", "groups", groups)
+    grepo.push_file("refs/meta/config", "project.config", acls)
+    grepo.push_file("refs/heads/master", ".gitreview", gitreview, True)
     sys.stdout.write("done.\n")
 
-
+def push_dir_in_gerrit_project(ssh_client, infos, target_dir, paths):
+    path_content = {}
+    for p in paths:
+        filename = os.path.basename(p)
+        with open(p) as fd:
+            content = fd.read()
+            path_content[filename] = content
+    sys.stdout.write("Clone created repo %s ... " % infos['name'])
+    email = "%s <%s>" % (infos['admin'], infos['email'])
+    grepo = GerritRepo(ssh_client, infos['name'], email)
+    sys.stdout.write("done.\n")
+    for filename, content in path_content.items():
+        path = os.path.join(target_dir, filename)
+        sys.stdout.write("Push %s on repo %s ... " %(path, infos['name']))
+        grepo.push_file("refs/heads/master", path, content)
+        sys.stdout.write("done.\n")
+    
 def init_redmine_project(redmine_client, infos):
     sys.stdout.write("Create project on Remine %s ... " % infos['name'])
     redmine_client.projects.new(name=infos['name'],
