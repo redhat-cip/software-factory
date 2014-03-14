8d3de358b5a1384ea018232899776a6e61373914
fabien boucher <fabien.boucher@enovance.com>
2014-03-14 17:30:04 +0000

    Change the way we do MySQL dump
    - Add autobackupmysql to the mysql role
    - Remove the little dump script
    
    Change-Id: I203643ae34d38270fb0e520f971bf59ae070f0de

--- puppet/modules/mysql/manifests/init.pp
+++ puppet/modules/mysql/manifests/init.pp
@@ -1,21 +1,2 @@
 class mysql () {
-    package {'cron':
-        ensure => 'installed',
-        name   => 'cron',
-    }
-
-    file {'/root/mysqldump.sh':
-        ensure  => file,
-        mode    => '700',
-        content => template('mysql/mysqldump.sh'),
-        require => [Package['cron']],
-    }
-
-    file {'/var/spool/cron/crontabs/root':
-        ensure => file,
-        mode   => '600',
-        content => '0 * * * * bash /root/mysqldump.sh
-',
-        require => [File['/root/mysqldump.sh']]
-    }
 }
