dcce30a0fb9e9dc1789030bacc3f2315e3ba420b
fabien boucher <fabien.boucher@enovance.com>
2014-04-02 11:13:51 +0000

    Add basics for functional tests (WIP)
    - Util class for managesf
    - Util class for Git/Git Review
    - Has not been tested yet
    
    Change-Id: Id709b7535625965e654418715efe77a3de4d6f78

--- tests/functional/test_gerrit.py
+++ tests/functional/test_gerrit.py
@@ -1,12 +1,40 @@
 #!/usr/bin/python
 
+import os
+import time
+import random
+
 from utils import Base
+from utils import ManageRestServer
+from utils import ManageSfUtils
+from utils import GerritGitUtils
+
+USER = 'fabien.boucher'
+PASSWD = 'userpass'
+EMAIL = 'fabien.boucher@enovance.com'
+PRIV_KEY_PATH = '/srv/SoftwareFactory/build/data/gerrit_admin_rsa'
+GERRIT_HOST = 'sf-gerrit:29418'
+
 
 class GerritFunctionalTests(Base):
-    def test_02_delete_repository(self):
-        print "Delete repository"
-        print "Check if it is deleted"
-    def test_01_create_repository(self):
-        print "Create repository"
-        print "Check if it is clonable"
+    @classmethod
+    def setUpClass(cls):
+        cls.mrs = ManageRestServer()
+        cls.mrs.run()
+        cls.msu = ManageSfUtils('localhost', 9090)
 
+    @classmethod
+    def tearDownClass(cls):
+        # We should first delete project that have been created
+        cls.mrs.stop()
+
+    def test_01_create_repository_as_admin(self):
+        pname = 'p%s' % random.randint(0, 1000)
+        # create the project as admin
+        self.msu.createProject(pname, USER, PASSWD)
+        # try to clone it as admin
+        ggu = GerritGitUtils(USER, PRIV_KEY_PATH, EMAIL)
+        url = "ssh://%s@%s/%s" % (USER, GERRIT_HOST, pname)
+        ggu.clone(url, pname)
+        # Check we were able to clone
+        self.assertTrue(os.path.isdir(os.path.join(ggu.tempdir, pname)))
--- tests/functional/utils.py
+++ tests/functional/utils.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python
 
+import os
 import unittest
 import subprocess
-import os
+import shlex
+import stat
+import tempfile
+import time
+from multiprocessing import Process
 from redmine import Redmine
 from requests.auth import HTTPBasicAuth
 from requests.exceptions import HTTPError
@@ -12,23 +17,101 @@ from pygerrit.rest import GerritRestAPI
 class Base(unittest.TestCase):
     pass
 
+
 class Tool:
-    cwd = "."
-    def run(self, argv):
-        old_cwd = os.getcwd()
-        os.chdir(self.cwd)
+    def __init__(self):
+        self.debug = file('/tmp/debug', 'a')
+        self.env = os.environ.copy()
+
+    def exe(self, cmd, cwd=None, wait=True):
+        cmd = shlex.split(cmd)
+        ocwd = os.getcwd()
+        if cwd:
+            os.chdir(cwd)
         try:
-            ret = subprocess.call([self.exe] + argv)
+            p = subprocess.Popen(cmd, stdout=self.debug,
+                                 stderr=subprocess.STDOUT,
+                                 env=self.env)
+            if wait:
+                p.wait()
         finally:
-            os.chdir(old_cwd)
-        return ret
+            os.chdir(ocwd)
+        return p
 
-class ManageSf(Tool):
-    cwd = "./tools/manage-sf"
-    exe = "./manage"
+class ManageRestServer(Tool):
+    def __init__(self):
+        Tool.__init__(self)
+        self.install_dir = "tools/managesf-ng/"
+        self.cmd = "pecan serve config.py"
 
-class Git(Tool):
-    exe = "/usr/bin/git"
+    def run(self):
+        self.env['PYTHONPATH'] = '.'
+        self.p = self.exe(self.cmd, self.install_dir, False)
+        time.sleep(2)
+
+    def stop(self):
+        self.p.terminate()
+
+
+class ManageSfUtils(Tool):
+    def __init__(self, host, port):
+        Tool.__init__(self)
+        self.host = host
+        self.port = port
+        self.install_dir = "tools/managesf-ng/cli"
+        self.base_cmd = "python sf-manage.py --host %s --port %s --auth %%s:%%s " \
+            % (self.host, self.port)
+
+    def createProject(self, name, user, passwd):
+        cmd = self.base_cmd % (user, passwd) + "create --name %s" % name
+        print cmd
+        self.exe(cmd, self.install_dir)
+
+    def deleteProject(self, name, user, passwd):
+        cmd = self.base_cmd % (user, passwd) + "delete --name %s" % name
+        self.exe(cmd, self.install_dir)
+
+
+class GerritGitUtils(Tool):
+    def __init__(self, user, priv_key_path, email):
+        Tool.__init__(self)
+        self.user = user
+        self.email = "%s <%s>" % (self.user, email)
+        self.priv_key_path = priv_key_path
+        self.tempdir = tempfile.mkdtemp()
+        ssh_wrapper = "ssh -o StrictHostKeyChecking=no -i " \
+                      "%s \"$@\"" % self.priv_key_path
+        wrapper_path = os.path.join(self.tempdir, 'ssh_wrapper.sh')
+        file(wrapper_path, 'w').write(ssh_wrapper)
+        os.chmod(wrapper_path, stat.S_IRWXU)
+        self.env['GIT_SSH'] = wrapper_path
+        self.env['GIT_COMMITTER_NAME'] = self.user
+        self.env['GIT_COMMITTER_EMAIL'] = self.email
+
+    def clone(self, uri, target):
+        assert uri.startswith('ssh://')
+        cmd = "git clone %s %s" % (uri, target)
+        self.exe(cmd, self.tempdir)
+        clone = os.path.join(self.tempdir, target)
+        assert os.path.isdir(clone)
+        return clone
+
+    def add_commit_in_branch(self, clone_dir, branch):
+        self.exe('git checkout master', clone_dir)
+        self.exe('git checkout -b %s' % branch)
+        file(os.path.join(clone_dir, 'testfile'), 'w').write('data')
+        self.exe('git add testfile', clone_dir)
+        self.exe("git commit --author '%s' testfile" % self.email, clone_dir)
+
+    def direct_push_branch(self, clone_dir, branch):
+        self.exe('git checkout %s' % branch, clone_dir)
+        self.exe('git push origin %s' % branch, clone_dir)
+        self.exe('git checkout master', clone_dir)
+
+    def review_push_branch(self, clone_dir, branch):
+        self.exe('git checkout %s' % branch, clone_dir)
+        self.exe('git review' % clone_dir)
+        self.exe('git checkout master', clone_dir)
 
 
 class GerritUtil:
