9ceb3373b2e812662e402d797207b3cadd74b034
Tristan Cacqueray <tristan.cacqueray@enovance.com>
2014-06-09 18:03:18 +0000

    Add a simple functional tests for jenkins: check config-* jobs
    Change-Id: I41f880e55fe380273ed614944bc4523278501252

---
+++ tests/functional/test_06_jenkins.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+#
+# Copyright (C) 2014 eNovance SAS <licensing@enovance.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import config
+import requests as http
+
+from utils import Base
+
+
+class TestJenkinsBasic(Base):
+    """ Functional tests to validate config repo bootstrap
+    """
+    def test_config_jobs_exist(self):
+        """ Test if jenkins config-update and config-check are created
+        """
+        r = http.get('%s/job/config-check' % config.JENKINS_SERVER)
+        self.assertEquals(r.status_code, 200)
+        r = http.get('%s/job/config-update' % config.JENKINS_SERVER)
+        self.assertEquals(r.status_code, 200)
