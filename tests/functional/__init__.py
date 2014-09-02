4d710ab6b8cc40180572262e4a5b4f0bb2e60d04
anil.venkata <anil.venkata@enovance.com>
2014-09-02 12:43:13 +0000

    remove create_gerrit_user from __init__.py
    This function is not needed in __init__.py,
    as users will be created in both redmine and gerrit
    while login for functional tests, by cauth server.
    
    Change-Id: I980e36c33833e9de707e61366ec9c40f09995672

--- tests/functional/__init__.py
+++ tests/functional/__init__.py
@@ -11,50 +11,5 @@ def assign_cookies():
                                       v['password'])
 
 
-def create_gerrit_user(user):
-    url = "%(gerrit_url)sa/accounts/%(user)s" % \
-          {'gerrit_url': config.GERRIT_SERVER,
-           'user': user}
-    admin_cookie = config.USERS[config.ADMIN_USER]['auth_cookie']
-
-    r = requests.get(url, cookies=dict(auth_pubtkt=admin_cookie))
-    if r.status_code == 200:
-        return
-
-    if r.status_code != 404:
-        raise Exception("Status code for %s: %s != 404" % (url, r.status_code))
-
-    # login to create an account
-    user_cookie = config.USERS[user]['auth_cookie']
-    url = "%(gerrit_url)slogin" % \
-          {'gerrit_url': config.GERRIT_SERVER}
-    requests.get(url, cookies=dict(auth_pubtkt=user_cookie),
-                 allow_redirects=False)
-
-    # set the name
-    url = "%(gerrit_url)sa/account/self/name" % \
-          {'gerrit_url': config.GERRIT_SERVER}
-    data = json.dumps({'name': user})
-    requests.put(url, cookies=dict(auth_pubtkt=user_cookie),
-                 allow_redirects=False,
-                 headers={"Content-Type": "application/json"},
-                 data=data)
-
-    url = "%(gerrit_url)sa/accounts/%(user)s/emails/%(email_id)s" % \
-          {'gerrit_url': config.GERRIT_SERVER,
-           'user': user,
-           'email_id': config.USERS[user]['email']}
-    data = json.dumps({'email': config.USERS[user]['email'],
-                       'no_confirmation': True,
-                       'preferred': True})
-    requests.put(url, cookies=dict(auth_pubtkt=admin_cookie),
-                 allow_redirects=False,
-                 headers={"Content-Type": "application/json"},
-                 data=data)
-
-
 def setup():
     assign_cookies()
-    for k, v in config.USERS.items():
-        if k != config.ADMIN_USER:
-            create_gerrit_user(k)
