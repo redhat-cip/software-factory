3e1f0668eb9359ac583427631b731f1242d13823
Fabien Boucher <fabien.boucher@enovance.com>
2014-09-30 11:14:41 +0000

    Managesf uses pysflib.sfgerrit.GerritUtils
    - Change managesf gerrit controller to user pysflib
    - Update managesf gerrit controller unittests
    - Add pysflib GerritUtils unittests
    
    Change-Id: I376f579de4bf003fd6d430da1d9e1c396a794cda

--- tools/managesf/managesf/controllers/utils.py
+++ tools/managesf/managesf/controllers/utils.py
@@ -14,72 +14,13 @@
 # under the License.
 
 from managesf import templates
-from pecan import abort, request, conf
-from cookielib import CookieJar
 from subprocess import Popen, PIPE
 from pwd import getpwnam
 from grp import getgrnam
 import os
-import requests as http
 import logging
 
 logger = logging.getLogger(__name__)
-
-
-def auth_cookie_in_jar(jar):
-    for cookie in jar:
-        if cookie.name == 'auth_pubtkt':
-            return {cookie.name: cookie.value}
-
-    return None
-
-
-def admin_auth_cookie():
-    if not conf.admin['cookiejar']:
-        conf.admin['cookiejar'] = CookieJar()
-
-    jar = conf.admin['cookiejar']
-    cookie = auth_cookie_in_jar(jar)
-    if not cookie:
-        url = "http://%(auth_host)s/auth/login" % \
-            {'auth_host': conf.auth['host']}
-        r = http.post(url, params={'username': conf.admin['name'],
-                                   'password': conf.admin['http_password'],
-                                   'back': '/'},
-                      allow_redirects=False)
-        for c in r.cookies:
-            if c.name == 'auth_pubtkt':
-                cookie = {c.name: c.value}
-            jar.set_cookie(c)
-
-    return cookie
-
-
-def send_request(url, expect_return,
-                 method='PUT',
-                 **kwargs):
-    meth = http.put
-    if method == 'GET':
-        meth = http.get
-    elif method == 'DELETE':
-        meth = http.delete
-    elif method == 'POST':
-        meth = http.post
-
-    if 'cookies' not in kwargs and \
-       request.cookies and 'auth_pubtkt' in request.cookies.keys():
-        kwargs['cookies'] = dict(auth_pubtkt=request.cookies['auth_pubtkt'])
-
-    resp = meth(url, allow_redirects=False, **kwargs)
-
-    if resp.status_code not in expect_return:
-        logger.debug("    Request " + method + " " + url +
-                     " failed with status code " +
-                     str(resp.status_code) + " - " + resp.text)
-
-        abort(resp.status_code)
-
-    return resp
 
 
 def template(t):
