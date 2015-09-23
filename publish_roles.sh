3a8a42c4c22bbd76be45fb6f8fc761d98b6f8540
Fabien Boucher <fabien.boucher@enovance.com>
2015-09-23 10:29:10 +0000

    Adapt publish_roles.sh script for credential plugin
    - No longer source the cred files
    - set +x when building the temp url
    - lower the delay to use the tempurl
    
    Related to that config change: I09fdcd7a96e9c2afc2f8a33a9ed863cf95924826
    
    Change-Id: Idd3c93707c44afd9789e4bca3300b913210dd46b

--- publish_roles.sh
+++ publish_roles.sh
@@ -2,9 +2,6 @@
 
 # publish code borrowed from publish_docs script
 
-# TODO: puppetize enocloud access
-source /etc/sf-dom-enocloud.openrc
-
 set -e
 set -x
 
@@ -24,7 +21,9 @@ for role_name in softwarefactory; do
     md5sum ${role}.edeploy | sudo tee ${role}.edeploy.md5
     for OBJECT in ${role}.edeploy ${role}.edeploy.md5 ${role}.md5 ${role}.img.qcow2 ${role}.img.qcow2.md5; do
         SWIFT_PATH="/v1/AUTH_${SWIFT_ACCOUNT}/${CONTAINER}/${OBJECT}"
-        TEMPURL=`swift tempurl PUT 3600 ${SWIFT_PATH} ${SWIFT_SECRET}`
+        set +x
+        TEMPURL=`swift tempurl PUT 120 ${SWIFT_PATH} ${TEMP_URL_KEY}`
+        set -x
         curl -f -i -X PUT --upload-file "$OBJECT" "${SWIFT_BASE_URL}${TEMPURL}" &> /dev/null && echo -n '.' || { echo 'Fail !'; exit 1; }
     done
 done
