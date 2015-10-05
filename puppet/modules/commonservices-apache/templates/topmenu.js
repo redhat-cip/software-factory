0c0a7d0f53e6b65fddf8db1605063b89424f840d
Tristan Cacqueray <tdecacqu@redhat.com>
2015-10-05 15:43:16 -0400

    Adds custom logo and favicon support
    This changes adds three new option to sfconfig (topmenu_logo_style,
    topmenu_logo_data and favicon_data) to store base64 encoded image content.
    
    Change-Id: I8f33e9b9892d6b943e4d5e2f5721ba8a34a84b10

--- puppet/modules/commonservices-apache/templates/topmenu.js
+++ puppet/modules/commonservices-apache/templates/topmenu.js
@@ -4,6 +4,13 @@ function loadTopmenu() {
         iframe.setAttribute("width", "100%");
         iframe.setAttribute("height", "51px");
         iframe.frameBorder = 0;
+        <% if scope.function_hiera(["favicon_data"]) %>
+            var favicon = document.createElement("link");
+            favicon.setAttribute("type", "image/x-icon");
+            favicon.setAttribute("href", "data:image/x-icon;base64,<%= scope.function_hiera(["favicon_data"]) %>");
+            favicon.setAttribute("rel", "icon");
+            document.getElementsByTagName("head")[0].appendChild(favicon);
+        <% end %>
         document.body.insertBefore(iframe, document.body.firstChild);
         document.title += ' [SF <%= scope.function_hiera(["sf_version"]) %>]';
 };
