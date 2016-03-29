a83c1b8a31348dddc73f67f2f5d78748bed4466a
Fabien Boucher <fboucher@redhat.com>
2016-03-29 13:06:11 +0000

    Playbook to rename project on SF
    Change-Id: I9fec75a692f6559725c881bbc80cc8c090b4f6cb

---
+++ tools/rename_project/rename-groups.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -ex
+
+[ -z "$1" ] && {
+    echo "Please provide old name as arg 1"
+    exit 1
+}
+[ -z "$2" ] && {
+    echo "Please provide new name as arg 2"
+    exit 1
+}
+
+ssh-keyscan -p 29418 managesf >> ~/.ssh/known_hosts
+ptl=$(ssh -i /root/sf-bootstrap-data/ssh_keys/gerrit_service_rsa -p 29418 admin@managesf gerrit ls-groups | grep "$2-ptl" || true)
+if [ -z "$ptl" ]; then
+    ssh -i /root/sf-bootstrap-data/ssh_keys/gerrit_service_rsa -p 29418 admin@managesf gerrit rename-group "$1-ptl" "$2-ptl"
+fi
+
+core=$(ssh -i /root/sf-bootstrap-data/ssh_keys/gerrit_service_rsa -p 29418 admin@managesf gerrit ls-groups | grep "$2-core" || true)
+if [ -z "$core" ]; then
+    ssh -i /root/sf-bootstrap-data/ssh_keys/gerrit_service_rsa -p 29418 admin@managesf gerrit rename-group "$1-core" "$2-core"
+fi
+
