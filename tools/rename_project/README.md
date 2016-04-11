b9184421ece20bdf9815265d57015495341e7988
Fabien Boucher <fboucher@redhat.com>
2016-04-11 12:46:22 +0200

    Minors fixes on the renaming project playbook
    Change-Id: Ib28b9b9c44da364f62ab09901e9c31ef420d65e5

--- tools/rename_project/README.md
+++ tools/rename_project/README.md
@@ -18,9 +18,3 @@ Some tags are available to split renaming actions:
 - project_rename: Rename project for Gerrit and Redmine
 - test_triggers_rename: Update project name in zuul/projects.yaml
 - replication_project_rename: Update project name/ssh config alias for replication
-
-Create that file before on the managesf node /root/.my.cnf
-
-[client]
-user=root
-password=<from sfcreds.yaml>
--- tools/rename_project/rename-groups.sh
+++ tools/rename_project/rename-groups.sh
@@ -11,7 +11,7 @@ set -ex
     exit 1
 }
 
-ssh-keyscan -p 29418 managesf >> ~/.ssh/known_hosts
+ssh-keyscan -p 29418 managesf | sed 's/managesf/\[managesf\]:29418/' >> ~/.ssh/known_hosts
 ptl=$(ssh -i /root/sf-bootstrap-data/ssh_keys/gerrit_service_rsa -p 29418 admin@managesf gerrit ls-groups | grep "$2-ptl" || true)
 if [ -z "$ptl" ]; then
     ssh -i /root/sf-bootstrap-data/ssh_keys/gerrit_service_rsa -p 29418 admin@managesf gerrit rename-group "$1-ptl" "$2-ptl"
