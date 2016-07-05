13d6c3caba61178e97f17dc34cad3be309611c0c
Fabien Boucher <fboucher@redhat.com>
2016-07-05 10:39:44 +0200

    SF - GUI test - deactivate test_create_project until it is stable
    This is safe to deactivate that test until we make it more stable.
    
    The following story has been created as a followup to reactivated
    it as soon as possible when we have stabilized it:
    
    http://softwarefactory-project.io/redmine/issues/1481
    
    Change-Id: I610c3b5d4b215164a8b9b0eb48c23585b6dd3c4d

--- tests/gui/user_stories/test_stories.py
+++ tests/gui/user_stories/test_stories.py
@@ -14,41 +14,41 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-from .base import BaseGuiTest, caption, snapshot_if_failure
-from .. import config
+# from .base import BaseGuiTest, caption, snapshot_if_failure
+# from .. import config
 
 
-class TestAdministratorTasks(BaseGuiTest):
-
-    @snapshot_if_failure
-    def test_create_project(self):
-        self.driver.get(config.GATEWAY_URL)
-        self.login_as(config.ADMIN_USER, config.ADMIN_PASSWORD)
-
-        msg = "Log in as adminstrator Go to the dashboard from the top menu"
-        with caption(self.driver, msg) as driver:
-            driver.get("%s/dashboard/" % config.GATEWAY_URL)
-
-        #TODO (gp) Give the buttons an element id
-        msg = "Click on 'Create project' bottom"
-        with caption(self.driver, msg) as driver:
-            driver.find_element_by_css_selector("button.btn-primary").click()
-
-        msg = "Describe your project"
-        with caption(self.driver, msg) as driver:
-            self.highlight("#projectname").send_keys("Demo_Project")
-            self.highlight("#description").send_keys("Test Description")
-            ele = self.highlight_button("Create project")
-            ele.click()
-
-        msg = "Now you have a project ready to work"
-        with caption(self.driver, msg) as driver:
-            self.highlight_link("Demo_Project").click()
-        msg = "Thank you for watching"
-        with caption(self.driver, msg) as driver:
-            self.highlight("body")
-
-
-if __name__ == '__main__':
-    from unittest import main
-    main()
+# class TestAdministratorTasks(BaseGuiTest):
+#
+#    @snapshot_if_failure
+#    def test_create_project(self):
+#        self.driver.get(config.GATEWAY_URL)
+#        self.login_as(config.ADMIN_USER, config.ADMIN_PASSWORD)
+#
+#        msg = "Log in as adminstrator Go to the dashboard from the top menu"
+#        with caption(self.driver, msg) as driver:
+#            driver.get("%s/dashboard/" % config.GATEWAY_URL)
+#
+#        #TODO (gp) Give the buttons an element id
+#        msg = "Click on 'Create project' bottom"
+#        with caption(self.driver, msg) as driver:
+#            driver.find_element_by_css_selector("button.btn-primary").click()
+#
+#        msg = "Describe your project"
+#        with caption(self.driver, msg) as driver:
+#            self.highlight("#projectname").send_keys("Demo_Project")
+#            self.highlight("#description").send_keys("Test Description")
+#            ele = self.highlight_button("Create project")
+#            ele.click()
+#
+#        msg = "Now you have a project ready to work"
+#        with caption(self.driver, msg) as driver:
+#            self.highlight_link("Demo_Project").click()
+#        msg = "Thank you for watching"
+#        with caption(self.driver, msg) as driver:
+#            self.highlight("body")
+#
+#
+# if __name__ == '__main__':
+#    from unittest import main
+#    main()
