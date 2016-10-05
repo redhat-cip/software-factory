db6eb6395ff9d0e92b6a62dc37dea32ff995bf9f
Tristan Cacqueray <tdecacqu@redhat.com>
2016-10-05 08:25:26 +0000

    health-check: import swiftlog test
    This change finishes the conversion of tests/integration into health-check.
    
    Closes-Bug: #1555
    Change-Id: I5e705c1ce71b1da968afec9be1a68c62e991f99e

--- functestslib.sh
+++ functestslib.sh
@@ -101,9 +101,13 @@ function run_health_openstack {
     export OS_AUTH_URL=${OS_AUTH_URL:-"http://192.168.42.1:5000/v2.0"}
     EXTRA_VARS="node=base_centos base_image_name=sf-latest os_slave_network=${HEAT_SLAVE_NETWORK}"
     EXTRA_VARS+=" os_auth_url=${OS_AUTH_URL} os_username=${OS_USERNAME} os_password=${OS_PASSWORD} os_tenant_name=${OS_TENANT_NAME}"
+    EXTRA_VARS+=" os_tempurl_key=${RANDOM}${RANDOM}${RANDOM}"
     ssh ${SF_HOST} ansible-playbook "--extra-vars='${EXTRA_VARS}'" /etc/ansible/health-check/nodepool.yaml >> ${ARTIFACTS_DIR}/integration_tests.txt \
         && echo "Nodepool integration test SUCCESS"    \
         || fail "Nodepool integration test failed" ${ARTIFACTS_DIR}/integration_tests.txt
+    ssh ${SF_HOST} ansible-playbook "--extra-vars='${EXTRA_VARS}'" /etc/ansible/health-check/swiftlogs.yaml >> ${ARTIFCATS_DIR}/integration_tests.txt \
+        && {echo "Swiftlogs integration test SUCCESS"; EXTRA_VARS+=" test_log_export=yes" }    \
+        || echo "Swiftlogs integration test failed (non-voting)"
     ssh ${SF_HOST} ansible-playbook "--extra-vars='${EXTRA_VARS}'" /etc/ansible/health-check/zuul.yaml >> ${ARTIFACTS_DIR}/integration_tests.txt \
         && echo "Zuul integration test SUCCESS"                        \
         || fail "Zuul integration test failed" ${ARTIFACTS_DIR}/integration_tests.txt
