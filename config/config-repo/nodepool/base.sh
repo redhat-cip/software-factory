dae15e87c45b657dcb35740596d2814b3410198d
Fabien Boucher <fboucher@redhat.com>
2016-11-07 11:40:51 +0100

    Synchronize nodepool/base.sh based on recent improvements
    Change-Id: I34631ece8dd6c9065b72aa32a7e2c7a0cfb974e8

--- config/config-repo/nodepool/base.sh
+++ config/config-repo/nodepool/base.sh
@@ -2,13 +2,24 @@
 
 set -xe
 
+function install_with_pip {
+    package=$1
+    dir=$(echo ${package}|sed -r 's/([a-zA-Z_-]+).*/\1/')
+    sudo yum install -y python-virtualenv
+
+    sudo virtualenv --system-site-packages --python=/usr/bin/python2 /opt/${dir}/venv
+    sudo /opt/${dir}/venv/bin/pip install --install-option="--install-scripts=/usr/bin" ${package}
+}
+
+
 sudo yum update -y > /dev/null
 
 # Base requirements
 sudo yum install -y epel-release > /dev/null
 sudo yum install -y python-pip git wget curl patch iproute > /dev/null
-sudo pip install --upgrade 'pip<8'
-sudo pip install tox
+
+install_with_pip 'pip<8'
+install_with_pip tox
 
 # The jenkins user. Must be able to use sudo without password
 sudo useradd -m jenkins
@@ -35,10 +46,14 @@ cat /opt/nodepool-scripts/authorized_keys | sudo tee -a /home/$cloud_user/.ssh/a
 # Install java (required by Jenkins)
 sudo yum install -y java
 
-# Install zuul_swift_upload and zuul-cloner
+# Install glob2 and python-magic for zuul_swift_upload
+sudo yum install -y python-magic
+sudo pip install glob2
+
+# Install zuul-cloner
 # TODO: replace this section by zuul package
 sudo yum install -y python-requests gcc python-devel python-crypto
-sudo pip install zuul glob2 python-magic
+install_with_pip zuul
 
 # Copy slave tools
 sudo cp -v /opt/nodepool-scripts/*.py /usr/local/bin/
