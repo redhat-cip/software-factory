f2c643e75c7e0031024f646e10a85f434e58d623
Nicolas Hicher <nhicher@redhat.com>
2016-11-10 14:23:36 -0500

    ansible: convert nodepool puppet module
    * convert erb template to jinja2
    * move service and logging files
    * remove puppet module
    
    Change-Id: I8827532bfb47296467acb971ef83e58237006f29

--- config/scripts/sf-update-hiera-config.py
+++ config/scripts/sf-update-hiera-config.py
@@ -140,6 +140,14 @@ def update_sfconfig(data):
         }
         dirty = True
 
+    # 2.2.7 rename variables for jinja2 templates
+    old_names = ["auth-url", "project-id", "max-servers", "boot-timeout"]
+    for value in data['nodepool']['providers']:
+        for name in old_names:
+            if name in value:
+                value[name.replace('-', '_')] = value.pop(name)
+                dirty = True
+
     return dirty
 
 
