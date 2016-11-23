8d669cb3efdca81d1c4e1726a64a30cd11903655
Fabien Boucher <fboucher@redhat.com>
2016-11-23 11:23:10 +0000

    Integrate repoXplorer as tech preview component
    RepoXplorer is small stats and charts utility
    for Git repositories.
    
    https://github.com/morucci/repoxplorer
    
    Change-Id: I648ef3e345f3876bdd694e07302cb8aa2dde97b2

--- run_functional-tests.sh
+++ run_functional-tests.sh
@@ -66,7 +66,7 @@ unset http_proxy
 unset https_proxy
 
 TECH_PREVIEW="elasticsearch job-logs-gearman-client job-logs-gearman-worker logstash kibana mirror"
-TECH_PREVIEW+=" storyboard storyboard-webclient"
+TECH_PREVIEW+=" storyboard storyboard-webclient repoxplorer"
 
 case "${TEST_TYPE}" in
     "minimal")
---
+++ tests/functional/test_repoxplorer.py
@@ -0,0 +1,53 @@
+#!/bin/env python
+#
+# Copyright (C) 2016 Red Hat
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import config
+import requests
+
+from utils import Base
+from utils import skipIfServiceMissing
+
+
+class TestRepoxplorer(Base):
+
+    @skipIfServiceMissing('repoxplorer')
+    def test_repoxplorer_accessible(self):
+        """ Test if RepoXplorer is accessible on gateway hosts
+        """
+        url = config.GATEWAY_URL + "/repoxplorer/"
+        resp = requests.get(url)
+        self.assertEqual(resp.status_code, 200)
+        self.assertTrue('[RepoXplorer] - Projects listing' in resp.text)
+
+    @skipIfServiceMissing('repoxplorer')
+    def test_repoxplorer_data_indexed(self):
+        """ Test if RepoXplorer has indexed the config repository
+        """
+        url = config.GATEWAY_URL + "/repoxplorer/commits.json?pid=%s" % (
+            config.GATEWAY_HOST)
+        resp = requests.get(url)
+        self.assertEqual(resp.status_code, 200)
+        self.assertTrue(resp.json()[2] > 0)
+
+    @skipIfServiceMissing('repoxplorer')
+    def test_repoxplorer_displayed_top_menu(self):
+        """ Test if RepoXplorer link is displayed in the top menu
+        """
+        url = config.GATEWAY_URL + "/topmenu.html"
+        resp = requests.get(url)
+        self.assertEqual(resp.status_code, 200)
+        self.assertTrue('href="/repoxplorer/"' in resp.text,
+                        'repoxplorer not present as a link')
