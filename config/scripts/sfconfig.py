9b7788748fa36107b9577a991b31831bdff4a41f
Tristan Cacqueray <tdecacqu@redhat.com>
2016-12-28 08:34:54 +0000

    gerrit: make the role generic
    This change refactor the gerrit and mysql role to use generic
    default variables:
    * Replace creds_ variable by new secrets
    * The mysql role creates databases based on a new (generic) dictionary
    
    Story: 40
    Change-Id: I379efd3c85278c3140083ba2a6a1c3a5b49a0596

--- config/scripts/sfconfig.py
+++ config/scripts/sfconfig.py
@@ -6,6 +6,7 @@
 import argparse
 import os
 import yaml
+import uuid
 
 
 def append_legacy(allvars_file, args):
@@ -25,18 +26,35 @@ def yaml_load(filename):
         return {}
 
 
+def yaml_dump(content, fileobj):
+    yaml.dump(content, fileobj, default_flow_style=False)
+
+
 def generate_role_vars(allvars_file, args):
     """ This function 'glue' all roles and convert sfconfig.yaml """
     secrets = yaml_load("%s/secrets.yaml" % args.lib)
+    arch = yaml_load(args.arch)
     sfconfig = yaml_load(args.sfconfig)
 
-    yaml.dump(secrets, open("%s/secrets.yaml" % args.lib, "w"))
-    # yaml.dump(secrets, allvars_file)
+    # Generate all variable when the value is CHANGE_ME
+    defaults = {}
+    for role in arch["roles"]:
+        role_vars = yaml_load("%s/roles/sf-%s/defaults/main.yml" % (
+                              args.ansible_root, role))
+        defaults.update(role_vars)
+        for key, value in role_vars.items():
+            if str(value).strip().replace('"', '') == 'CHANGE_ME':
+                if key not in secrets:
+                    secrets[key] = str(uuid.uuid4())
 
-    # Set absolute urls and hostnames
-    glue = {}
+    # Generate dynamic role variable in the glue dictionary
+    glue = {'mysql_databases': {}}
 
-    # Fix url used in services
+    def get_hostname(role):
+        if len(arch["roles"][role]) != 1:
+            raise RuntimeError("Role %s is defined on multi-host" % role)
+        return arch["roles"][role][0]["hostname"]
+
     glue["gateway_url"] = "https://%s" % sfconfig["fqdn"]
 
     if sfconfig["debug"]:
@@ -44,7 +62,33 @@ def generate_role_vars(allvars_file, args):
             glue["%s_loglevel" % service] = "DEBUG"
             glue["%s_root_loglevel" % service] = "INFO"
 
-    yaml.dump(glue, allvars_file, default_flow_style=False)
+    if "mysql" in arch["roles"]:
+        glue["mysql_host"] = get_hostname("mysql")
+
+    if "managesf" in arch["roles"]:
+        glue["managesf_internal_url"] = "http://%s:%s" % (
+            get_hostname("managesf"), defaults["managesf_port"])
+
+    if "gerrit" in arch["roles"]:
+        glue["gerrit_pub_url"] = "%s/r/" % glue["gateway_url"]
+        glue["gerrit_internal_url"] = "http://%s:%s/r/" % (
+            get_hostname("gerrit"), defaults["gerrit_port"])
+        glue["gerrit_email"] = "gerrit@%s" % sfconfig["fqdn"]
+        glue["gerrit_mysql_host"] = glue["mysql_host"]
+        glue["mysql_databases"]["gerrit"] = {
+            'hosts': list(set(('localhost',
+                               get_hostname("gerrit"),
+                               get_hostname("managesf")))),
+            'user': 'gerrit',
+            'password': secrets['gerrit_mysql_password'],
+        }
+
+    # Save secrets to new secrets file
+    yaml_dump(secrets, open("%s/secrets.yaml" % args.lib, "w"))
+    # And add them to the all.yaml file
+    yaml_dump(secrets, allvars_file)
+    # Add glue to the all.yaml.file
+    yaml_dump(glue, allvars_file)
 
 
 def usage():
