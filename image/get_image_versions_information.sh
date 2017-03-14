27ddc2c52d90529406af9f761ed06de4290b04ea
Tristan Cacqueray <tdecacqu@redhat.com>
2017-03-14 05:42:14 +0000

    zuul: use package
    Depends-On: Ib14f3ddc7b8dffe4dadedaebbf0824132dd48fdf
    Change-Id: Ic19504401229645a3164148d64d1c44bb838db5d

--- image/get_image_versions_information.sh
+++ image/get_image_versions_information.sh
@@ -19,14 +19,8 @@ echo
 echo "# Gem packages versions"
 sudo chroot ${dir} gem list | grep '^[a-z]' | sort | while read pkg; do echo "gem: ${pkg}"; done
 echo
-echo "# Zuul venv"
-sudo chroot ${dir} /srv/zuul/bin/pip freeze | sort | while read pkg; do echo "zuul_pip: ${pkg}"; done
-echo
 echo "# Nodepool venv"
 sudo chroot ${dir} /srv/nodepool/bin/pip freeze | sort | while read pkg; do echo "nodepool_pip: ${pkg}"; done
-echo
-echo "# Managesf venv"
-sudo chroot ${dir} /var/www/managesf/bin/pip freeze | sort | while read pkg; do echo "managesf_pip: ${pkg}"; done
 
 # Nodejs package listing disable because npm stuck when system is not running
 #echo
